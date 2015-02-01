618f58e95b21edc5b8807d3e27ca994b3fe67320
Lucas <lucasvm@uw.edu>
2015-02-01 14:38:09 -0800

    added different map selections

--- guess_where.js
+++ guess_where.js
@@ -16,15 +16,15 @@ seattle = new city(
 	"Seattle, WA", 
 	'http://i166.photobucket.com/albums/u116/lucasvanmeter/seattle_zps1nvuscxp.jpg', 
 	["seattle", "seattle, wa"], 
-	"You are there right now.")
+	"That's a big lake to the east and not even a lake on the west.")
 newhaven = new city(
 	"New Haven, CT", 
 	'http://i166.photobucket.com/albums/u116/lucasvanmeter/new_haven_zpsliqaxxls.jpg', 
 	["new haven", "new haven, ct"], 
-	"You won't get this one.")
+	"This one might be hard. Yale is in this city.")
 tijuana = new city(
 	"Tijuana, Mexico", 
-	'http://i166.photobucket.com/albums/u116/lucasvanmeter/tijuana_zpsgltcgq5a.jpg',
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/tijuana_zpsldesdywa.jpg',
 	["tijuana", "tijuana, mexico"], 
 	"There are a lot more cars going north than south.")
 giza = new city(
@@ -36,22 +36,111 @@ boston = new city(
 	"Boston, MA", 
 	'http://i166.photobucket.com/albums/u116/lucasvanmeter/boston_zpstqmrnfjg.jpg', 
 	["boston"], 
-	"Famous city on the east coast.")
+	"It's a famous city on the east coast.")
 santiago = new city(
 	"Santiago de Compostela, Spain", 
 	'http://i166.photobucket.com/albums/u116/lucasvanmeter/santiago_de_compostela_zps0qnprrrm.jpg', 
-	["santiago de compostela","santiago"], 
-	"You really won't get this one.")
+	["santiago de compostela","santiago", "santiago de compostela, spain", "santiago, spain"], 
+	"A famous pilgrimage ends here.")
+paris = new city(
+	"Paris, France", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/paris_zpslhqb9bxq.jpg', 
+	["paris","paris, france"], 
+	"It's in Europe")
+volunteer = new city(
+	"Volunteer park, Seattle", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/volunteerpark_zps281jm9ih.jpg', 
+	["volunteer","volunteer park"], 
+	"It's in Seattle.")
+versailles = new city(
+	"Versailles, France", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/versailles_zps3fmtmdsb.jpg', 
+	["versailles","versailles, france"], 
+	"That is one big garden.")
+seward = new city(
+	"Seward park, Seattle", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/seward_zpshuzwy7hh.jpg', 
+	["seward","seward park"], 
+	"It's in Seattle.")
+revenna = new city(
+	"Revenna park, Seattle", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/revenna_zpsii8dqjwq.jpg', 
+	["revenna","revenna park"], 
+	"It's in Seattle.")
+seoul = new city(
+	"Seoul, Korea", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/seoul_zpsnx06etsp.jpg', 
+	["seoul","seoul, korea"], 
+	"It's a famous city in southeast asia.")
+lasvagas = new city(
+	"Las Vagas, NV", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/lasvagas_zpswl0mym9z.jpg', 
+	["las vagas","las vagas, nv"], 
+	"That strip down the middle is very built up but not much around it is.")
+presido = new city(
+	"Presido park, San Fransisco", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/presido_zpswqz7toud.jpg', 
+	["presido","presido park"], 
+	"It's in San Fransico.")
+sanfransisco = new city(
+	"San Fransisco, CA", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/sanfran_zpsqulismb8.jpg', 
+	["san fransisco","san fransisco, ca", "san fran"], 
+	"Long brideges, very dense.")
+mttabor = new city(
+	"Mt. Tabor, Portland", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/mttabor_zpsvyualnvq.jpg', 
+	["mt tabor","mt. tabor", "mt tabor park", "mt. tabor park"], 
+	"It's in Portland.")
+centralpark = new city(
+	"Central park, New York", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/centralpark_zps7axrraos.jpg', 
+	["central","central park"], 
+	"The most famous park in the United States.")
+goldengate = new city(
+	"Golden Gate park, San Fransisco", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/godlengatepark_zpsuqdqpu9n.jpg', 
+	["golden gate","golden gate park"], 
+	"It's in San Fransisco.")
+forestpark = new city(
+	"Forest park, Portland", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/forestpark_zpspugf1lrc.jpg', 
+	["forset park","forest"], 
+	"It's in Portland.")
+laurelhurst = new city(
+	"Laurelhurst park, Portland", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/larualhurst_zpswtivyjgt.jpg', 
+	["laurelhurst","laurelhurst park"], 
+	"It's in Portland.")
+discovery = new city(
+	"Discovery park, Seattle", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/discoverypark_zpsxpmvhnpu.jpg', 
+	["discovery","discovery park"], 
+	"It's in Seattle.")
+buenosaires = new city(
+	"Buenos Aires, Argentina", 
+	'http://i166.photobucket.com/albums/u116/lucasvanmeter/buenosaires_zpse1blgpjz.jpg', 
+	["buenos aires"], 
+	"A famous city in South America.")
 
+var allList = [seattle, sanfransisco, portland, buenosaires, boston, giza, versailles, newhaven, tijuana, paris, santiago, lasvagas, seoul]
+var usList = [seattle, sanfransisco, portland, boston, newhaven, tijuana, lasvagas]
+var worldList = [giza, versailles, santiago, paris, buenosaires, tijuana, seoul]
+var parkList = [discovery, goldengate, centralpark, revenna, mttabor, seward, laurelhurst, presido, volunteer]
+var games = [allList, usList, worldList, parkList]
 
-var usaList = [portland, seattle, newhaven, tijuana, boston]
-var worldList = [giza, santiago]
-
-function game(cities) {
+function game(lists) {
 	var self = this
-	this.cities = cities
+	this.lists = lists
+	this.cities = this.lists[0]
 	this.score = 0
 	this.cityTracker = self.cities.length
+
+	this.newGame = function(num) {
+		self.cities = self.lists[num]
+		self.cityTracker = self.cities.length
+		self.nextCity()
+	}
 
 	this.updatePic = function(pic) {
 		$('#pic').attr('src',pic)
@@ -132,7 +221,7 @@ function game(cities) {
 
 //The buttons in the game
 
-game = new game(usaList)
+game = new game(games)
 
 $(document).ready(function() {
 
@@ -152,5 +241,21 @@ $(document).ready(function() {
 			game.getInput(input)
 			$("#guess").val("")
 		}
+	})
+
+	$("#all").click(function() {
+		game.newGame(0)
+	})
+
+	$("#us").click(function() {
+		game.newGame(1)
+	})
+
+	$("#world").click(function() {
+		game.newGame(2)
+	})
+
+	$("#parks").click(function() {
+		game.newGame(3)
 	})
 })
